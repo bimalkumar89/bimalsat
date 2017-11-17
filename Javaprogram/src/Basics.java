911d4b71f754214f5ab3961be15514e98a7db027
bimalkumar89 <33723530+bimalkumar89@users.noreply.github.com>
2017-11-17 19:31:59 +0530

    Commit the Program
    bg

---
+++ Javaprogram/src/Basics.java
@@ -0,0 +1,20 @@
+
+public class Basics {
+
+	public static void main(String[] args) {         // actual execution start from main method
+		
+		//System.out.println("Hi ");
+//double a=3.898;
+//double b=9.99;
+//double sum=a+b;
+//System.out.println("sum is"+sum);   //+ concatination operator
+
+		Basics2 obj=new Basics2();
+		obj.validateh();
+		System.out.println(obj.validateh());
+
+
+
+	}
+
+}
---
+++ Javaprogram/src/Basics2.java
@@ -0,0 +1,23 @@
+
+public class Basics2 {
+
+	//public void validateh(){
+		
+	//System.out.println("Header is validated");
+	
+
+//	}
+	public String validateh(){
+		
+		System.out.println("Header is validated");
+		return "Pass";
+
+	}
+//public int validateh(){
+		
+	
+//	System.out.println("Header is validated");
+//		return 2;
+
+//		}
+}
